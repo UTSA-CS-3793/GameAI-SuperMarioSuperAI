4e82ce4c4a9eb8580e424d7a8fe98ceaa787e014
bmburkhardt <bmburkhardt@alaska.edu>
2018-04-27 22:34:41 -0500

    pushing project files

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # GameAI-SuperMarioSuperAI
 Spring 2018 - Repository for Team Tensor-Dough
+
+Virtual Box Image
+=================
+	I have set up a virtual machine image with all the dependencies and files already installed to make it easier if you do not want set to set up the environment on your machine.
+
+	A download for the image can be found here:
+		link
+
+	Steps to execute:
+	- Import the image into virtual box
+	- Boot it up and log in. The user password is "1234"
+	- cd Desktop/Tensor-dough/gym-rle
+	- To train the model
+	  - python mario_train.py
+	- To run an already trained model
+	  - python mario_run.py
+
+
+Manual Environment Setup
+========================
+
+	Follow the steps in the linked PDF:
+	- https://drive.google.com/file/d/1N39pOupAee0x1MUa5E57lyTwSdjqUbrA/view?usp=sharing
+
+
+Dependencies
+============
+	- Linux Ubuntu 16.04 (may work with other version but untested)
+	- Python 2.7
+	  - https://www.python.org/download/releases/2.7/
+	- Tensorflow 1.7 (works with or without GPU support)
+	  - https://www.tensorflow.org/install/
+	- Retro Learning Environment (RLE)
+	  - https://github.com/nadavbh12/Retro-Learning-Environment
+	- libsdl1.2-dev | libsdl-gfx1.2-dev | libsdl-image1.2-dev | cmake
+	  - sudo apt-get install libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake
+	- Gym Retro Learning Environment (Gym-RLE)
+	  - https://github.com/nadavbh12/gym-rle
+	- pip 8.1.1
+	  - sudo apt-get install python-pip
+	- numpy 1.11.0
+	  - sudo apt-get install python-numpy
+	- Super Mario World ROM
+	  - https://drive.google.com/file/d/154rlFZGZHU2fjPF96K6gzPp9Uszk11k8/view?usp=sharing
+	- Keras 2.1.6
+	  - sudo pip install keras
+	- Keras-rl 0.3.1
+	  - sudo pip install keras-rl==0.3.1
+	- Pillow
+	  - sudo pip install pillow
+	- h5py
+	  - sudo pip install h5py
+
+
+
+
---
+++ mario_run.py
@@ -0,0 +1,124 @@
+import numpy as np
+import gym
+import gym_rle
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Convolution2D, MaxPooling2D, Dropout, Permute
+from keras.optimizers import Adam
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import LinearAnnealedPolicy ,EpsGreedyQPolicy#BoltzmannQPolicy
+from rl.memory import SequentialMemory
+from rl.core import Processor
+
+from PIL import Image
+
+FILE_NAME='trained_data.h5f'
+ENV_NAME = 'SuperMarioWorld-v0'
+INPUT_SHAPE = (224, 256)#Size of image given by observation
+WINDOW_LENGTH = 4
+
+# Get the environment and extract the number of actions.
+env = gym.make(ENV_NAME)
+np.random.seed(123)
+env.seed(123)
+nb_actions = env.action_space.n #number of possible action actor can take
+
+#Custom Processor to make this actually work
+class MarioProcessor(Processor):
+    def process_observation(self, observation):
+        assert observation.ndim == 3  # (height, width, channel)
+        img = Image.fromarray(observation)
+        img = img.resize(INPUT_SHAPE).convert('L')  # resize and convert to grayscale
+        processed_observation = np.array(img)
+        processed_observation = processed_observation.T
+        assert processed_observation.shape == INPUT_SHAPE
+        return processed_observation.astype('uint8')  # saves storage in experience memory
+
+    def process_state_batch(self, batch):
+        # We could perform this processing step in `process_observation`. In this case, however,
+        # we would need to store a `float32` array instead, which is 4x more memory intensive than
+        # an `uint8` array. This matters if we store 1M observations.
+        processed_batch = batch.astype('float32') / 255.
+        return processed_batch
+
+    def process_reward(self, reward):
+        return np.clip(reward, -1., 1.)
+
+
+
+# Next, we build a very simple model regardless of the dueling architecture
+# if you enable dueling network in DQN , DQN will build a dueling network base on your model automatically
+# Also, you can build a dueling network by yourself and turn off the dueling network in DQN.
+input_shape = (WINDOW_LENGTH,)+INPUT_SHAPE
+
+model = Sequential()
+model.add(Permute((2,3,1), input_shape=input_shape))
+model.add(Convolution2D(32, 8, 8, subsample=(4, 4)))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, 4, 4, subsample=(2, 2)))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, 3, 3, subsample=(1, 1)))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
+print(model.summary())
+
+
+# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+# even the metrics!
+memory = SequentialMemory(limit=10000, window_length=WINDOW_LENGTH)
+processor = MarioProcessor()
+policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05, nb_steps=1000)
+#policy = BoltzmannQPolicy(tau=1.)
+# enable the dueling network
+# you can specify the dueling_type to one of {'avg','max','naive'}
+#dqn = DQNAgent(model=model, nb_actions=nb_actions, processor = processor, memory=memory, nb_steps_warmup=10,
+#               enable_dueling_network=True, dueling_type='avg', target_model_update=1e-2, policy=policy)
+dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory, enable_dueling_network=True, dueling_type='avg',
+               processor=processor, nb_steps_warmup=10, gamma=.99, target_model_update=1000,train_interval=4, delta_clip=1.)
+
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+#This is okay for now, but put an if statement here after this(Unless the command below is commented out)
+dqn.load_weights('trained_data.h5f')
+
+# Okay, now it's time to learn something! We visualize the training here for show, but this
+# slows down training quite a lot. You can always safely abort the training prematurely using
+# Ctrl + C.
+#dqn.fit(env, nb_steps=70000, visualize=False, verbose=1, log_interval=1000)
+
+# After training is done, we save the final weights.
+#dqn.save_weights('duel_dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=False)
+
+
+# Finally, evaluate our algorithm for 5 episodes.
+dqn.test(env, nb_episodes=5, visualize=True, nb_max_episode_steps=500)
+
+
+#old model
+'''
+model.add(Conv2D(32, 3, activation='relu'))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+
+
+#If it stops at 80k again then use this:
+dqn.load_weights(FILE_NAME)
+dqn.fit()
+dqn.save_weights(FILE_NAME, overwrite=False)
+dqn.test()
+'''
